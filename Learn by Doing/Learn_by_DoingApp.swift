b930149e58a816979b0cf77c9f0e965b14fecb38
Ghenadie Isacenco <isacenco@gmail.com>
2025-10-25 17:53:27 +0200

    Project Setup

---
+++ Learn by Doing/Learn_by_DoingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_by_DoingApp.swift
+//  Learn by Doing
+//
+//  Created by Ghenadie Isacenco on 25/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Learn_by_DoingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
